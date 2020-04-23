77f6eaa945f3997e4a94154b8ba5a24090de7710
Andrew Szymanek <andrewszymanek@Andrews-MacBook-Pro.local>
2020-04-23 14:24:58 -0500

    added scaffold, appbar, center, and other widgets with northern lights image

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -4,8 +4,17 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey[600],
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage('https://images.unsplash.com/photo-1531366936337-7c912a4589a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'),
+          ),
+        ),
       ),
     ),
   );
